0c0ac3436509bbfb91e3e50eff8ae2f4a6ecd380
Chris Mims <chritopherm970@aol.com>
2020-08-05 20:01:21 -0500

    Fixed issue with db name.

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,37 @@
+$(function() {
+    $(".btn").on("click", function(event) {
+      var id = $(this).data("id");
+
+  
+      // Send the PUT request.
+      $.ajax("/api/burger/" + id, {
+        type: "PUT",
+      }).then(
+        function() {
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBurger = {
+        burger_name: $("ca").val().trim(),
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burger", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+})
