5d69b89a9e57bba3fb92c2a4654dd4596dd98570
Chris Mims <chritopherm970@aol.com>
2020-08-06 22:23:32 -0500

    Fixed issue with burgers not showing up.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,5 +1,4 @@
 var express = require("express");
-
 var router = express.Router();
 
 var burger = require("../models/burger.js");
@@ -14,24 +13,24 @@ router.get("/", function (req, res) {
     })
 })
 
-router.post("/api/burger", function (req, res) {
+router.post("/api/burgers", function (req, res) {
     burger.create([
         "burger_name", "devoured"
     ], [
-        req.body.burger_name, false
+        req.body.burger_name, req.body.devoured
     ], function (result) {
         // Send back the ID of the new quote
         res.json({ id: result.insertId });
     });
 });
 
-router.put("/api/burger/:id", function (req, res) {
+router.put("/api/burgers/:id", function (req, res) {
     var condition = "id = " + req.params.id;
 
     console.log("condition", condition);
 
     burger.update({
-        devoured: true
+        devoured: req.body.devoured
     }, condition, function (result) {
     if (result.changedRows == 0) {
             // If no rows were changed, then the ID must not exist, so 404
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,11 +1,16 @@
 $(function() {
-    $(".btn").on("click", function(event) {
+    $(".change-devoured").on("click", function(event) {
       var id = $(this).data("id");
-
+      var newDevoured = $(this).data("newdevoured");
+      
+      var newDevouredState = {
+        devoured: newDevoured
+      }
   
       // Send the PUT request.
-      $.ajax("/api/burger/" + id, {
+      $.ajax("/api/burgers/" + id, {
         type: "PUT",
+        data: newDevouredState
       }).then(
         function() {
           // Reload the page to get the updated list
@@ -13,17 +18,17 @@ $(function() {
         }
       );
     });
-  
     $(".create-form").on("submit", function(event) {
       // Make sure to preventDefault on a submit event.
       event.preventDefault();
   
       var newBurger = {
-        burger_name: $("ca").val().trim(),
+        burger_name: $("#ca").val().trim(),
+        devoured: $("[name=devoured]:checked").val().trim()
       };
   
       // Send the POST request.
-      $.ajax("/api/burger", {
+      $.ajax("/api/burgers", {
         type: "POST",
         data: newBurger
       }).then(
